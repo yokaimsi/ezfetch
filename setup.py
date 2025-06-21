c508fec76fdda255a1bce64bd7f43c8b50140a69
Yokai <104296229+yokaimsi@users.noreply.github.com>
2025-06-21 19:14:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="ezfetch",
-    version="1.0.3",
-    description="A clean, Python-based system info tool like Neofetch",
+    version="1.0.5",
+    description="A minimal yet powerful system information tool",
     author="yokaimsi",
     packages=find_packages(),
     install_requires=["psutil"],
     entry_points={
-        "console_scripts": [
-            "ezfetch = ezfetch.main:display_info"
-        ]
+        'console_scripts': [
+            'ezfetch=ezfetch.main:display_info',
+        ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
